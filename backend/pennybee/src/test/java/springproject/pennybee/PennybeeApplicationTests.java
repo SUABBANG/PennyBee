7ce270a2223d67436a379dae0a2424ce0159f127
SUABBANG <suabbang@gmail.com>
2025-01-16 16:38:24 +0900

    startproject

---
+++ backend/pennybee/src/test/java/springproject/pennybee/PennybeeApplicationTests.java
@@ -0,0 +1,13 @@
+package springproject.pennybee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PennybeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
